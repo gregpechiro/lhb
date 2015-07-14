be7ae7e2028becb9b0e656ca63786671b73b5381
gabewitmer <gabenwitmer@gmail.com>
2015-07-14 14:19:38 -0400

    fixed service page

--- services.php
+++ services.php
@@ -24,7 +24,6 @@
 					<br><br>
 					<p>We serve everything within an hour radius of the following areas: </p>
 					<ul>
-						<li>1hr radius</li>
 						<li>York</li>
 						<li>Chester</li>
 						<li>Lancaster</li>
@@ -35,10 +34,11 @@
 				<div class="col-md-9 col-xs-12">
 					<div class="row">
 						<div class="col-sm-4">
-							<img class="col-sm-12 col-xs-11" src="images/samples/480x480/image_01.jpg" alt="...">
+							<h2 class="text-underline no-margin-top visible-xs text-center">New Homes</h2>
+							<img class="col-sm-12 col-xs-12" src="images/samples/480x480/image_01.jpg" alt="...">
 						</div>
 						<div class="col-sm-8 col-xs-12 text-center">
-							<h2 class="text-underline">New Homes</h2>
+							<h2 class="text-underline no-margin-top hidden-xs">New Homes</h2>
 							<p>
 								Suspendisse imperdiet nisi quam, a molestie elit tempor sit amet. Quisque a diam sit amet est accumsan venenatis in in mauris. Mauris ultrices tempus quam id mollis. Donec felis eros, dignissim sit amet cursus quis, fringilla et turpis. Maecenas eros eros, tempor ut feugiat a, ullamcorper at nisl. Etiam maximus ipsum sed vulputate sagittis. Nam varius non diam vel efficitur. Suspendisse potenti. Cras lacinia, mi eget vulputate ultrices, nulla mi bibendum velit, sed condimentum tortor ipsum a mauris.
 							</p>
@@ -46,43 +46,47 @@
 					</div>
 					<br>
 					<div class="row">
-						<div class="col-sm-4 visible-xs visible-sm">
-							<img class="col-sm-12 col-xs-11" src="images/samples/480x480/image_02.jpg" alt="...">
+						<div class="col-sm-4 visible-xs ">
+							<h2 class="text-underline no-margin-top text-center">Decks</h2>
+							<img class="col-sm-12 col-xs-12" src="images/samples/480x480/image_02.jpg" alt="...">
 						</div>
 						<div class="col-sm-8 col-xs-12 text-center">
-							<h2 class="text-underline">Decks</h2>
+							<h2 class="text-underline no-margin-top hidden-xs">Decks</h2>
 							<p>
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at commodo lacus. In egestas iaculis felis, eget volutpat arcu condimentum non. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla non nulla ac sapien pretium tincidunt. Duis pharetra augue eu nunc feugiat faucibus. Sed vel turpis eget tellus faucibus rhoncus. Sed nec semper leo, id efficitur sem. Suspendisse potenti. Suspendisse potenti. In a felis diam. Morbi id turpis metus. Aliquam faucibus, mi vel mattis dignissim, lectus eros fringilla tortor, a blandit dolor diam et quam. Donec gravida est vel augue pellentesque, id venenatis nibh ultrices.
 							</p>
 						</div>
-						<div class="col-sm-4 hidden-sm hidden-xs">
-							<img class="col-sm-12 col-xs-11" src="images/samples/480x480/image_02.jpg" alt="...">
+						<div class="col-sm-4  hidden-xs">
+							<img class="col-sm-12 col-xs-12" src="images/samples/480x480/image_02.jpg" alt="...">
 						</div>
 					</div>
 					<br>
 					<div class="row">
 						<div class="col-sm-4">
-							<img class="col-sm-12 col-xs-11" src="images/samples/480x480/image_03.jpg" alt="...">
+							<h2 class="text-underline no-margin-top visible-xs text-center">Nadu Excavating</h2>
+							<img class="col-sm-12 col-xs-12" src="images/samples/480x480/image_03.jpg" alt="...">
 						</div>
 						<div class="col-sm-8 col-xs-12 text-center">
-							<h2 class="text-underline">Nadu Excavating</h2>
+							<h2 class="text-underline no-margin-top hidden-xs">Nadu Excavating</h2>
 							<p>
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at commodo lacus. In egestas iaculis felis, eget volutpat arcu condimentum non. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla non nulla ac sapien pretium tincidunt. Duis pharetra augue eu nunc feugiat faucibus. Sed vel turpis eget tellus faucibus rhoncus. Sed nec semper leo, id efficitur sem. Suspendisse potenti. Suspendisse potenti. In a felis diam. Morbi id turpis metus. Aliquam faucibus, mi vel mattis dignissim, lectus eros fringilla tortor, a blandit dolor diam et quam. Donec gravida est vel augue pellentesque, id venenatis nibh ultrices.
 							</p>
 						</div>
 					</div>
+					<br>
 					<div class="row">
-						<div class="col-sm-4 visible-xs visible-sm">
-							<img class="col-sm-12 col-xs-11" src="images/samples/480x480/image_04.jpg" alt="...">
+						<div class="col-sm-4 visible-xs">
+							<h2 class="text-underline no-margin-top text-center">M&J Plumbing</h2>
+							<img class="col-sm-12 col-xs-12" src="images/samples/480x480/image_04.jpg" alt="...">
 						</div>
 						<div class="col-sm-8 col-xs-12 text-center">
-							<h2 class="text-underline">M&J Plumbing</h2>
+							<h2 class="text-underline no-margin-top hidden-xs">M&J Plumbing</h2>
 							<p>
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at commodo lacus. In egestas iaculis felis, eget volutpat arcu condimentum non. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla non nulla ac sapien pretium tincidunt. Duis pharetra augue eu nunc feugiat faucibus. Sed vel turpis eget tellus faucibus rhoncus. Sed nec semper leo, id efficitur sem. Suspendisse potenti. Suspendisse potenti. In a felis diam. Morbi id turpis metus. Aliquam faucibus, mi vel mattis dignissim, lectus eros fringilla tortor, a blandit dolor diam et quam. Donec gravida est vel augue pellentesque, id venenatis nibh ultrices.
 							</p>
 						</div>
-						<div class="col-sm-4 hidden-sm hidden-xs">
-							<img class="col-sm-12 col-xs-11" src="images/samples/480x480/image_04.jpg" alt="...">
+						<div class="col-sm-4 hidden-xs">
+							<img class="col-sm-12 col-xs-12" src="images/samples/480x480/image_04.jpg" alt="...">
 						</div>
 					</div>
 				</div>

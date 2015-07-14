ea97f48ad85b185558d98b1e4c7c1df3e26d2b12
gabewitmer <gabenwitmer@gmail.com>
2015-07-14 10:36:02 -0400

    fixed js

--- js/quote.js
+++ js/quote.js
@@ -8,7 +8,7 @@ var quotes = [
 (function getQuote() {
     var q = quotes[Math.floor((Math.random() * quotes.length))];
     document.getElementById('randomQuote').innerHTML = q;
-    document.getElementById('randomQuote2').innerHTML = q;
+    // document.getElementById('randomQuote2').innerHTML = q;
     // document.getElementById('randomQuote3').innerHTML = q;
     // document.getElementById('randomQuote4').innerHTML = q;
 })();

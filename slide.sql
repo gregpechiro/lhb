0a8840b5ead1f22b6ddfa8e54a6cda7780cd2ebe
gabewitmer <gabenwitmer@gmail.com>
2015-07-22 12:37:33 -0400

    added logout to nav

--- slide.sql
+++ slide.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE `slide` (
   `id` int(6) NOT NULL AUTO_INCREMENT,
-  `titlw` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
   `body` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 )
--- stubs/adminNav.php
+++ stubs/adminNav.php
@@ -19,6 +19,7 @@
                 <li><a href="about.php"> About Us </a></li>
                 <li><a href="contact.php"> Contact Us </a></li>
                 <li><a href="services.php"> Services </a></li>
+                <li><a id="cancel" href="#"> Logout </a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
